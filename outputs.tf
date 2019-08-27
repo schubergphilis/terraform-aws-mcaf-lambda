61d548eae632b3c43befdf33f2a0e121ffed9dd7
Tommy Maintz <tommy.maintz@gmail.com>
2019-08-27 14:42:37 +0200

    Add support for deploying a lambda to a VPC
    Signed-off-by: Tommy Maintz <tommy.maintz@gmail.com>

--- outputs.tf
+++ outputs.tf
@@ -7,3 +7,8 @@ output "qualified_arn" {
   value       = aws_lambda_function.default.qualified_arn
   description = "Qualified ARN of the lambda"
 }
+
+output "security_group_id" {
+  value       = var.subnet_ids != null ? aws_security_group.default[0].id : ""
+  description = "If the Lambda is deployed into a VPC this will output the security group id"
+}

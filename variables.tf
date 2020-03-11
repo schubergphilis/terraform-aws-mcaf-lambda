51627514e82f23bd07227c0eb962c151dc62257b
Nemanja Banovic <nbanovic@schubergphilis.com>
2020-03-11 11:28:53 +0100

    changed description

--- variables.tf
+++ variables.tf
@@ -18,7 +18,7 @@ variable "cloudwatch_logs" {
 variable "cloudwatch_logs_retention" {
   type        = number
   default     = 14
-  description = "Specifies the number of days you want to retain log events in the specified log group. Possible values are: 1, 3, 5, 7, 14, 30, 60, 90, 120, 150, 180, 365, 400, 545, 731, 1827, and 3653."
+  description = "Number of days you want to retain log events in the specified log group"
 }
 
 variable "environment" {

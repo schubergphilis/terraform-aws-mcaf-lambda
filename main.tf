8bef7fcfe452bb5a9199e1015777c674e7224eba
Tommy Maintz <tommy.maintz@gmail.com>
2021-05-20 15:58:22 +0200

    Put the IAM policy in alphabetical order

--- main.tf
+++ main.tf
@@ -16,7 +16,7 @@ data "aws_iam_policy_document" "default" {
 
     principals {
       type        = "Service"
-      identifiers = ["lambda.amazonaws.com", "edgelambda.amazonaws.com"]
+      identifiers = ["edgelambda.amazonaws.com", "lambda.amazonaws.com"]
     }
   }
 }

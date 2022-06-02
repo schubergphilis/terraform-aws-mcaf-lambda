46ec7ee88d29fccc1fb35da1e24ebc3b1b16edba
rogiermccv <73637162+rogiermccv@users.noreply.github.com>
2022-06-02 16:00:42 +0200

    fix sg reference for egress rule

--- main.tf
+++ main.tf
@@ -75,7 +75,7 @@ resource "aws_security_group_rule" "allow_all_egress" {
   to_port           = 0
   protocol          = "-1"
   cidr_blocks       = ["0.0.0.0/0"] #tfsec:ignore:aws-vpc-no-public-egress-sg
-  security_group_id = aws_security_group.default.id
+  security_group_id = aws_security_group.default[0].id
 }
 
 data "archive_file" "dummy" {
